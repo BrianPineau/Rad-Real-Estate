f2ed7dd4a25de9fc4a8ea8043794c84cf1686caf
Brian <pinoub@gmail.com>
2020-03-23 23:29:09 -0500

    Header and API call set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+const App = () => {
+  const APP_KEY = 'X1-ZWz1hm2z4ieoej_7kib9';
+  const exampleReq = `http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${APP_KEY}`;
+  
+  return(
+    <div className="AppHeader">
+      <header>
+        <div className="header-top-row">
+          <div className="top-left-menu">
+            <ul>
+              <li>Buy</li>
+              <li>Rent</li>
+              <li>Sell</li>
+              <li>Home Loans</li>
+              <li>Agent Finder</li>
+            </ul>
+          </div>
+
+          <div className="site-icon">
+            Meep!
+          </div>
+
+          <div className="top-right-menu">
+            <ul>
+              <li>Manage Rentals</li>
+              <li>Advertise</li>
+              <li>My Zillow</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="header-bottom-row">
+          <form>
+            <input className="bottom-item" type="text" />
+            <button className="bottom-item" type="button">For Sale</button>
+            <button className="bottom-item" type="button">Price</button>
+          </form>
+          <aside>24 Saved Homes</aside>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
